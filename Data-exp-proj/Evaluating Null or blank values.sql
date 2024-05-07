c62cdaf88a87af4fab7117f389d348698636a6c2
kamibrenda <mukamibrenda254@gmail.com>
2024-05-07 16:05:04 +0300

    Data-exp-proj/Evaluating Null or blank values.sql

--- Data-exp-proj/Evaluating Null or blank values.sql
+++ Data-exp-proj/Evaluating Null or blank values.sql
@@ -1,12 +1,11 @@
---to show null values
-SELECT total_laid_off
-FROM Layoffs_staging2
---WHERE total_laid_off IS NULL;
-
-SELECT COUNT(*)
-FROM Layoffs_staging2
-WHERE total_laid_off IS NULL;
+----to show null values
+--SELECT *
+--FROM Layoffs_staging2
+--WHERE total_laid_off IS NULL
 
 SELECT *
 FROM Layoffs_staging2
-WHERE LTRIM(RTRIM(total_laid_off)) = '';
+WHERE total_laid_off IS NULL OR total_laid_off = '';
+
+
+
