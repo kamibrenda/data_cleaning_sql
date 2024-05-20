b5e1975e06244bad60371c85d600d926390068f6
kamibrenda <mukamibrenda254@gmail.com>
2024-05-20 13:54:57 +0300

    Columns Drop

--- Data-exp-proj/Drop Columns.sql
+++ Data-exp-proj/Drop Columns.sql
@@ -1,10 +1,13 @@
---Removing any unecessary columns
---Deleting null values of 2 columns
+--Removing any unecessary columns had to use the OR statement to enable data exploration due to the fact that NULL values were being picked out when finding the MAX values
+Deleting null values of 2 columns
 DELETE 
 FROM Layoffs_staging2
 WHERE total_laid_off = 'NULL'
-AND percentage_laid_off = 'NULL'
+OR percentage_laid_off = 'NULL'
 
 --removing row_num column fromm the table
 ALTER TABLE layoffs_staging2
 DROP COLUMN row_num
+
+
+
